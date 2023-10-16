98aa7735d22f60faf05825c76c45ff34b47d622b
AgiwalAa <aakashagiwal001@gmail.com>
2023-10-16 10:25:34 +0530

    Updates

---
+++ precache-manifest.35831cfda80a72619553441433e906e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323c52525b08112b1bef1fb1cceddafd",
+    "url": "/far-away-list/index.html"
+  },
+  {
+    "revision": "43f06016b989f15eb7e7",
+    "url": "/far-away-list/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/far-away-list/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/far-away-list/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "43f06016b989f15eb7e7",
+    "url": "/far-away-list/static/js/main.525da30a.chunk.js"
+  },
+  {
+    "revision": "51ebbae6a946576fc114",
+    "url": "/far-away-list/static/js/runtime~main.28671c32.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/far-away-list/static/media/logo.06e73328.svg"
+  }
+]);
